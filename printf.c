6f13b37fd2c32d7536760765519bb096c8596d58
Ikrame97 <133514945+Ikrame97@users.noreply.github.com>
2023-07-26 01:16:53 +0100

    hello

--- printf.c
+++ printf.c
@@ -8,9 +8,9 @@
  */
 int _printf(const char *format, ...)
 {
-	int sm = 0;
+	int sum = 0;
 	va_list ap;
-	char *z, *strt;
+	char *x, *start;
 	params_t params = PARAMS_INIT;
 
 	va_start(ap, format);
@@ -19,31 +19,31 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (z = (char *)format; *z; z++)
+	for (x = (char *)format; *x; x++)
 	{
 		init_params(&params, ap);
-		if (*z != '%')
+		if (*x != '%')
 		{
-			sm += _putchar(*z);
+			sum += _putchar(*x);
 			continue;
 		}
-		strt = z;
-		z++;
-		while (get_flag(z, &params)) /* while char at p is flag char */
+		start = x;
+		x++;
+		while (get_flag(x, &params)) /* while char at p is flag char */
 		{
-			z++; /* next char */
+			x++; /* next char */
 		}
-		z = get_width(z, &params, ap);
-		z = get_precision(z, &params, ap);
-		if (get_modifier(z, &params))
-			z++;
-		if (!get_specifier(z))
-			sm += print_from_to(start, z,
-				params.l_modifier || params.h_modifier ? z - 1 : 0);
+		x = get_width(x, &params, ap);
+		x = get_precision(x, &params, ap);
+		if (get_modifier(x, &params))
+			x++;
+		if (!get_specifier(x))
+			sum += print_from_to(start, x,
+				params.l_modifier || params.h_modifier ? x - 1 : 0);
 		else
-			sm += get_print_func(z, ap, &params);
+			sum += get_print_func(x, ap, &params);
 	}
 	_putchar(BUF_FLUSH);
 	va_end(ap);
-	return (sm);
+	return (sum);
 }
